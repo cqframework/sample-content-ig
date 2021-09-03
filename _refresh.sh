8b7eeb14ac01a6b2f8ad1f83dd71fc74f0efb8d3
Adam Stevenson <stevenson_adam@yahoo.com>
2021-09-03 13:03:47 -0600

    updates to refresh scripts

--- _refresh.sh
+++ _refresh.sh
@@ -2,8 +2,7 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
-resources_path=$PWD/input/resources
-ig_resource_path=./input/anc-cds.xml
+ig_ini_path=$PWD/ig.ini
 
 set -e
 echo Checking internet connection...
@@ -22,15 +21,13 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
+	JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
+		JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
 fi
-
-#sh input/pagecontent/quick-start-bundles/_refreshQuickStart.sh
