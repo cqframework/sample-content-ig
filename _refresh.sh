67d3a9affb8f83bf07281d67e5720694b1c1710a
Bryn Rhodes <brynrhodes@users.noreply.github.com>
2021-10-11 13:52:40 -0600

    Fixed case-sensitivity of JAVA command in refresh

--- _refresh.sh
+++ _refresh.sh
@@ -21,12 +21,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
+	java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
+		java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
