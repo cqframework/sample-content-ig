6542dee3e57d4c3549804641a9c082a2ad7bf6b9
Bryn Rhodes <brynrhodes@users.noreply.github.com>
2021-11-13 14:30:45 -0700

    Update _updateCQFTooling.bat

--- _updateCQFTooling.bat
+++ _updateCQFTooling.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET "dlurl=https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.opencds.cqf&a=tooling&v=1.3.1&c=jar-with-dependencies"
+SET "dlurl=https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=org.opencds.cqf&a=tooling&v=1.3.1&c=jar-with-dependencies"
 SET tooling_jar=tooling-1.3.1-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache\
 SET skipPrompts=false
