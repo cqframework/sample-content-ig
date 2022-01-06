1ac7f6f28e5273d20c7067cd53283dcfb4e4e9db
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2022-01-06 11:34:03 -0700

    Update refresh tooling to 1.4.0 release

--- _autoPublish.bat
+++ _autoPublish.bat
@@ -1,5 +1,5 @@
 @ECHO off
-SET tooling_jar=tooling-1.3.1-jar-with-dependencies.jar
+SET tooling_jar=tooling-1.4.0-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
 SET resources_path=%~dp0input/resources
 SET ig_resource_path=input/anc-cds.xml
